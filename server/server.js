7928cd51a4c0164a0581b15c0d25b61766bf4852
Jenifer <85919892+jenifer98k@users.noreply.github.com>
2025-02-13 22:56:48 +0530

    Update server.js trying to sole the network error

--- server/server.js
+++ server/server.js
@@ -18,7 +18,17 @@ const allowedOrigins = [
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 
 // API Endpoints
 app.get("/", (req, res) => res.send("API Working"));
